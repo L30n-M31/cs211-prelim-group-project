6c204e4e236f99bc81a21733ef4e73a44249e7a0
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-11 21:34:30 +0800

    added methods and instance variables

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,4 +1,66 @@
+/**
+ * @author Leonhard Leung
+ * Date: 09/11/2023
+ */
+
 package sort;
 
-public class SelectionSort {
-}
+public class SelectionSort implements Sorter {
+    public long bestCount;
+    public long aveCount;
+    public long worstCount;
+
+    @Override
+    public long sort(String[] array) {
+        long initializeCounter = 0, incrementCounter = 0, conditionCounter = 0, blockCounter = 0, returnCounter = 1;
+
+        for (int i = 0; i < array.length - 1; i++, incrementCounter++, conditionCounter++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++, incrementCounter++, conditionCounter++) {
+                if (array[minIndex].compareToIgnoreCase(array[j]) > 0) {
+                    minIndex = j;
+                    blockCounter++;
+                }
+                conditionCounter++;
+            }
+            String temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+
+            initializeCounter++;
+            conditionCounter++;
+            blockCounter += 4;
+        }
+        initializeCounter++;
+        conditionCounter++;
+
+        return initializeCounter + incrementCounter + conditionCounter + blockCounter + returnCounter;
+    } // end of sort method
+
+    @Override
+    public void count(String[] bestArray, String[] aveArray, String[] worstArray) {
+        bestCount = sort(bestArray);
+        aveCount = sort(aveArray);
+        worstCount = sort(worstArray);
+    } // end of count method
+
+    @Override
+    public long getBestCount() {
+        return bestCount;
+    }
+
+    @Override
+    public long getAveCount() {
+        return aveCount;
+    }
+
+    @Override
+    public long getWorstCount() {
+        return worstCount;
+    }
+
+    @Override
+    public String getSortType() {
+        return "SELECTION";
+    }
+} // end of SelectionSort class
